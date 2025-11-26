87d185db60f219bd227a6df0c3c763bea09a061b
hayden.smith@justice.gov.uk <hayden.smith@justice.gov.uk>
2025-11-26 09:50:03 +0000

    New formatting of forward look

--- forward_look.R
+++ forward_look.R
@@ -12,6 +12,8 @@ library(stringr)
 library(openxlsx)
 library(lubridate)
 library(tidyr)
+library(lubridate)
+options(lubridate.week.start = 1)
 
 
 # <------------------------------------------------------- FUNCTIONS ------------------------------------------------------->
@@ -35,12 +37,12 @@ get_info <- function(listitems) {
 # <------------------------------------------------------- MAIN CODE ------------------------------------------------------->
 search_url <- "https://www.gov.uk/search/research-and-statistics?content_store_document_type=upcoming_statistics&organisations=ministry-of-justice&order=release-date-oldest"
 pages      <- process_html(search_url, ".govuk-pagination__link-label") %>%
-      html_text()
+  html_text()
 
 if (length(pages) == 0) {
-    pages <- "1"
+  pages <- "1"
 } else {
-    pages <- str_replace(pages,"2 of ","")
+  pages <- str_replace(pages,"2 of ","")
 }
 
 for (i in 1:as.numeric(pages)) {
@@ -51,7 +53,7 @@ for (i in 1:as.numeric(pages)) {
     html_attr("href")
   publication_name <- process_html(search_url, ".gem-c-document-list") %>% 
     html_nodes("a") %>%
-    html_text()
+    html_text(trim=TRUE)
   pub_attribs      <- process_html(search_url, ".gem-c-document-list__item")
   
   pub_attribs <- t(as.data.frame(lapply(pub_attribs,get_info)))
@@ -63,22 +65,44 @@ for (i in 1:as.numeric(pages)) {
                              "publication.date.temp",
                              "publication.status")
   
-  prerelease <- as.data.frame(cbind(publication_name,publication_url,pub_attribs))
+  publication_desc <- character()
+  for(j in publication_url){
+    publication_desc_t <- process_html(paste0("https://www.gov.uk", j), ".govuk-grid-row") %>% html_nodes(".gem-c-lead-paragraph") %>%
+      html_text(trim=TRUE)
+    publication_desc <- c(publication_desc, publication_desc_t)
+  }
+  
+  prerelease <- as.data.frame(cbind(publication_name,publication_url, publication_desc, pub_attribs))
   
   if (i == 1) {
-      prerelease_all <- prerelease
+    prerelease_all <- prerelease
   } else {
-      prerelease_all <- rbind(prerelease_all,prerelease)
+    prerelease_all <- rbind(prerelease_all,prerelease)
   }
   
 }
 
+prerelease_all_t <- prerelease_all %>% mutate(long_title = sub("[[:space:]]*[,:[:digit:]].*$", "", publication_name))
 
-prerelease_all$publication.date.temp <- stringr::str_remove(prerelease_all$publication.date.temp," 9:30am")
-prerelease_all$publication.date.temp <- stringr::str_remove(prerelease_all$publication.date.temp," 9:03am")
-prerelease_all$publication.date.temp <- stringr::str_remove(prerelease_all$publication.date.temp," 10:00am")
+for (i in seq_along(prerelease_all_t$long_title)) {
+  if (grepl("(His|Her|HM).*Prison.*Probation Service", prerelease_all_t$long_title[i], ignore.case = TRUE)) {
+    rest <- sub("(?i)(His|Her|HM).*Prison.*Probation Service", "", prerelease_all_t$long_title[i], perl = TRUE)
+    prerelease_all_t$long_title[i] <- paste("HMPPS", trimws(rest))
+  }
+}
 
-prerelease_all2 <- prerelease_all %>%
+
+#Combine forward look with publication leads
+lookup_wb <- read.xlsx("Stats Publication Leads.xlsx") %>% select(long_title, `Publication.Month(s)`, `Lead.Contact`, G6, G7, `Justice.Data`, Mailbox)
+prerelease_all_t <- prerelease_all_t %>% 
+  mutate(long_title=str_squish(str_to_lower(long_title))) %>%
+  left_join(lookup_wb %>% mutate(long_title=str_squish(str_to_lower(long_title))), by="long_title") 
+
+prerelease_all_t$publication.date.temp <- stringr::str_remove(prerelease_all_t$publication.date.temp," 9:30am")
+prerelease_all_t$publication.date.temp <- stringr::str_remove(prerelease_all_t$publication.date.temp," 9:03am")
+prerelease_all_t$publication.date.temp <- stringr::str_remove(prerelease_all_t$publication.date.temp," 10:00am")
+
+prerelease_all2 <- prerelease_all_t %>%
   mutate(publication.date = 
            case_when(
              grepl("^[[:digit:]]+", publication.date.temp) == TRUE ~ publication.date.temp,
@@ -88,39 +112,80 @@ prerelease_all2 <- prerelease_all %>%
              TRUE ~ paste0("31 ", publication.date.temp)
            ),
          Week = lubridate::isoweek(dmy(publication.date)),
-         Year = lubridate::year(dmy(publication.date))) %>%
+         Year = lubridate::isoyear(dmy(publication.date))) %>%
   select(-publication.date.temp)
 
-dates <- as.data.frame(format(seq(as.Date("2 January 2023", format = "%d %b %Y"), as.Date("27 December 2026", format = "%d %b %Y"), by="days"), format="%d %b %Y"))
+start_date <- floor_date(Sys.Date() - years(1), "week")
+end_date <- Sys.Date() + years(3)
 
-dates_new <- dates %>%
-  mutate(Week = rep(1:52, each = 7, times = 4)) %>%
-  mutate(week_num2 = rep(1:7, each = 1, times = nrow(dates)/7)) %>%
-  rename("Week_commences" = 1) %>%
-  mutate(Year = as.numeric(substr(Week_commences, nchar(Week_commences)-4, nchar(Week_commences))))%>%
-  filter(week_num2 == 1)
+dates <- tibble(
+  Week_commences = seq(start_date, end_date, by="week"),
+  Week = isoweek(Week_commences),
+  Year = isoyear(Week_commences),
+  Month = month(Week_commences, label=TRUE, abbr=FALSE)
+)
+
+prerelease_weeks <- dates %>%
+  full_join(prerelease_all2, by=c("Week", "Year")) %>%
+  arrange(Year, Week)
+
+rowvector <- seq_len(nrow(prerelease_weeks))
+current_week <- floor_date(Sys.Date(), unit="week", week_start=1)
+
+start_index <- which(prerelease_weeks$Week_commences==current_week)
+if(length(start_index)==0) {
+  start_index <- min(rowvector[!is.na(prerelease_weeks$publication.date)])
+} else{
+  start_index <- min(start_index)
+}
+end_index <- max(rowvector[!is.na(prerelease_weeks$publication.date)])
+
+prerelease_all <- prerelease_weeks[start_index:end_index, ]
+
+#Add in month headers for readability 
+prerelease_all <- prerelease_all %>%
+  mutate(
+    Month = factor(
+      Month, 
+      levels=c("January", "February", "March", "April", "May", "June", "July", 
+               "August", "September", "October", "November", "December"),
+      ordered=TRUE
+    )
+  ) %>%
+  group_by(Year, Month) %>%
+  group_split() %>%
+  lapply(function(g) {
+    month_chr <- as.character(unique(g$Month))
+    year_val <- unique(g$Year)
+    header_txt <- paste0(month_chr, " ", year_val)
+    g <- g %>% mutate(Week_commences = format(Week_commences, "%a %d %b %Y"))
+    m_header <- tibble(
+      Week_commences = header_txt,
+      Year = year_val,
+      Month = month_chr
+    )
+    bind_rows(m_header, g)
+  }) %>%
+  bind_rows() %>%
+  select(-c(Year, Month, long_title)) %>%
+  mutate(publication.date=format(as.Date(publication.date, format="%d %b %Y"), "%a %d %b %Y"))
 
 
-prerelease_weeks <- dates_new %>%
-  full_join(prerelease_all2 %>% filter(publication.status != "cancelled"), by = c("Week" = "Week",
-                                                                                  "Year" = "Year")) %>%
-  arrange(Year,Week) %>%
-  select(Week, Year, publication_name, publication_url, publication.type, publication.department, publication.date, publication.status, Week_commences)
-
-
-rowvector      <- 1:nrow(prerelease_weeks)
-prerelease_all <- prerelease_weeks[min(rowvector[!is.na(prerelease_weeks$publication.date)]):
-                                     max(rowvector[!is.na(prerelease_weeks$publication.date)]),]
-
-names(prerelease_all) <- c("Week",
-                           "Year",
+names(prerelease_all) <- c("Week Commencing",
+                           "Week",
                            "Publication Title", 
                            "Announcement URL",
+                           "Description",
                            "Statistics Type",
                            "Department",
-                           "Publication Date",
                            "Status",
-                           "Week Commencing")
+                           "Usual publication month(s)",
+                           "Lead contact",
+                           "Grade 6",
+                           "Grade 7",
+                           "Justice Data",
+                           "Mailbox",
+                           "Publication Date")
 
 # for testing purposes
 # prerelease_all = rbind(prerelease_all, c(6, 2024, "Experimental release"))
@@ -132,18 +197,18 @@ keywords <- list("ad-hoc", "ad hoc", "experimental")
 prerelease_all['Type'] <- NA
 
 for (row in 1:nrow(prerelease_all)) {
-    if (is.na(prerelease_all$`Publication Title`[row])) {
-        prerelease_all$Type[row] <- NA
-    } else {  
-        for (i in 1:length(keywords)) {
-            if (grepl(keywords[i], tolower(prerelease_all$`Publication Title`[row]), fixed = TRUE)) {
-                prerelease_all$Type[row] <- (gsub(" ", "-", keywords[i]))
-                break
-            } else {
-                prerelease_all$Type[row] <- "standard"
-            }
-        }
+  if (is.na(prerelease_all$`Publication Title`[row])) {
+    prerelease_all$Type[row] <- NA
+  } else {  
+    for (i in 1:length(keywords)) {
+      if (grepl(keywords[i], tolower(prerelease_all$`Publication Title`[row]), fixed = TRUE)) {
+        prerelease_all$Type[row] <- (gsub(" ", "-", keywords[i]))
+        break
+      } else {
+        prerelease_all$Type[row] <- "standard"
+      }
     }
+  }
 }
 
 # <---------------------------------------------------- CREATE WORKBOOK ---------------------------------------------------->
@@ -153,52 +218,90 @@ class(govuk_link) <- "hyperlink"
 
 wb <- openxlsx::createWorkbook()
 openxlsx::addWorksheet(wb, "Forward Look")
+modifyBaseFont(wb, fontSize = 9, fontColour = "#000000", fontName = "Arial")
 openxlsx::writeData(wb, "Forward Look","MoJ Statistics Forward Look",
                     startRow = 1)
-openxlsx::writeData(wb,"Forward Look", paste("This list contains a week-by-week view of  MoJ Official and National Statistics that have been pre-announced on the gov.uk release calendar as at",
-                                            format(Sys.Date(),"%d %B %Y")), startRow = 2)
+openxlsx::writeData(wb,"Forward Look", paste("This list contains a week-by-week view of regular MoJ Official Statistics releases that have been pre-announced on the gov.uk release calendar. The list is updated every week on a Friday."), startRow = 2)
 openxlsx::writeData(wb,"Forward Look", govuk_link,startRow = 3)
+openxlsx::writeData(wb, "Forward Look", paste("Last updated: ", format(Sys.Date(),"%A %d %B %Y")), startRow=4)
+
 selections <- c("Week Commencing",
                 "Publication Title",
                 "Publication Date",
                 "Status",
                 "Week",
-                "Type")
-openxlsx::writeData(wb,"Forward Look", select(prerelease_all, all_of(selections)), startRow = 4)
+                "Type",
+                "Usual publication month(s)", 
+                "Lead contact",
+                "Grade 7",
+                "Grade 6",
+                "Mailbox",
+                "Justice Data")
+openxlsx::writeData(wb,"Forward Look", select(prerelease_all, all_of(selections)), startRow = 6)
 
-titleStyle    <- createStyle(fontSize = 14, textDecoration = "bold")
+arialStyle    <- createStyle(fontName="Arial")
+titleStyle    <- createStyle(fontSize = 16, textDecoration = "bold")  
 subtitleStyle <- createStyle(fontSize = 12)
 linkStyle     <- createStyle(fontSize = 12, valign = "top")
-evenStyle     <- createStyle(bgFill = "#b4c6e7")
-oddStyle      <- createStyle(bgFill = "#d9e1f2")
-hideStyleEven <- createStyle(bgFill = "#b4c6e7", fontColour = "#b4c6e7")
-hideStyleOdd  <- createStyle(bgFill = "#d9e1f2", fontColour = "#d9e1f2")
-border        <- createStyle(border="top", borderColour = "#FFFFFF")
+#evenStyle     <- createStyle(bgFill = "#b4c6e7")
+#oddStyle      <- createStyle(bgFill = "#d9e1f2")
+hideStyleEven <- createStyle(bgFill = "#FFFFFF", fontColour = "#FFFFFF")
+hideStyleOdd  <- createStyle(bgFill = "#FFFFFF", fontColour = "#FFFFFF")
+border        <- createStyle(border="top", borderColour = "#000000")
+border_left   <- createStyle(border="left", borderColour= "#000000")
+wrap_style    <- createStyle(wrapText = TRUE, valign="center")
+m_titleStyle  <- createStyle(fontSize = 10, textDecoration = "bold", bgFill="#D9D9D9", valign="center")
+bold_st       <- createStyle(textDecoration = "bold", fontSize=9)
+bold_st2       <- createStyle(textDecoration = "bold", fontSize=12)
+conf          <- createStyle(bgFill = "#C6EFCE", fontColour = "#006100")
+prov          <- createStyle(bgFill = "#FFEB9C", fontColour = "#9C5700")
+canc          <- createStyle(bgFill = "#FFC7CE", fontColour = "#9C0006")
 
-conditionalFormatting(wb, "Forward Look", cols = 1:6, rows = 1:nrow(prerelease_all)+4,
-                      rule = "=AND(LEN($E5)>0,MOD(RIGHT($E5,2),2)=0)", style = evenStyle)
-conditionalFormatting(wb, "Forward Look", cols = 1:6, rows = 1:nrow(prerelease_all)+4,
-                      rule = "=AND(LEN($E5)>0,MOD(RIGHT($E5,2),2)=1)", style = oddStyle)
-conditionalFormatting(wb, "Forward Look", cols = 1, rows = 1:nrow(prerelease_all)+4,
-                      rule = "=AND(LEN($E5)>0,MOD(RIGHT($E5,2),2)=0,$E5=$E4)", style = hideStyleEven)
-conditionalFormatting(wb, "Forward Look", cols = 1, rows = 1:nrow(prerelease_all)+4,
-                      rule = "=AND(LEN($E5)>0,MOD(RIGHT($E5,2),2)=1,$E5=$E4)", style = hideStyleOdd)
-conditionalFormatting(wb, "Forward Look", cols = 1:6, rows = 1:nrow(prerelease_all)+4,
-                      rule = "=AND($E5<>$E4)", style = border)
+#conditionalFormatting(wb, "Forward Look", cols = 1:7, rows = 1:nrow(prerelease_all)+6,
+#                      rule = "=AND(LEN($E7)>0,MOD(RIGHT($E7,2),2)=0)", style = evenStyle)
+#conditionalFormatting(wb, "Forward Look", cols = 1:7, rows = 1:nrow(prerelease_all)+6,
+#                      rule = "=AND(LEN($E7)>0,MOD(RIGHT($E7,2),2)=1)", style = oddStyle)
+conditionalFormatting(wb, "Forward Look", cols = 1:12, rows = 7:(nrow(prerelease_all)+7),
+                      rule = '=AND($A7<>$A6)', style = border)
+conditionalFormatting(wb, "Forward Look", cols=1:13, rows=7:(nrow(prerelease_all)+6),
+                      rule = '=LEFT($A7, 3)="MON"', style=border_left)
+conditionalFormatting(wb, "Forward Look", cols=1:12, rows=7:(nrow(prerelease_all)+6),
+                      rule = '=LEFT($A7,3)<>"MON"', style=m_titleStyle, stack=TRUE)
+conditionalFormatting(wb, "Forward Look", cols=2, rows=7:(nrow(prerelease_all)+6), 
+                      rule = '=AND($B7<>"")', style=bold_st)
+conditionalFormatting(wb, "Forward Look", cols = 1, rows = 1:nrow(prerelease_all)+6,
+                      rule = "=AND(LEN($E7)>0,MOD(RIGHT($E7,2),2)=0,$E7=$E6)", style = hideStyleEven)
+conditionalFormatting(wb, "Forward Look", cols = 1, rows = 1:nrow(prerelease_all)+6,
+                      rule = "=AND(LEN($E7)>0,MOD(RIGHT($E7,2),2)=1,$E7=$E6)", style = hideStyleOdd)
+conditionalFormatting(wb, "Forward Look", cols=4, rows=7:(nrow(prerelease_all)+6),
+                      rule = '=AND($D7="confirmed")', style=conf)
+conditionalFormatting(wb, "Forward Look", cols=4, rows=7:(nrow(prerelease_all)+6),
+                      rule = '=AND($D7="provisional")', style=prov)
+conditionalFormatting(wb, "Forward Look", cols=4, rows=7:(nrow(prerelease_all)+6),
+                      rule = '=AND($D7="cancelled")', style=canc)
 
-setColWidths(wb, 1, cols = c(1:6), widths=c(18,"auto",30,10,10), hidden=c(rep(FALSE,4),TRUE))
+setColWidths(wb, 1, cols = c(1:12), widths=c(25,80,25,10,10,10,30,30,30,20,45,15), hidden=c(rep(FALSE,4),TRUE, TRUE, rep(FALSE,6)))
 setRowHeights(wb, 1, 3, 30)
 
-header_st <- createStyle(fgFill = "#1F497D", textDecoration = "Bold", fontColour = "#FFFFFF")
-cell_st   <- createStyle(halign = "left")
+hdr_rows <- which(!grepl("Mon", prerelease_all$`Week Commencing`)) + 6 
+week_rows <- which(grepl("Mon", prerelease_all$`Week Commencing`) & !grepl("Offender management statistics", prerelease_all$`Publication Title`)) + 6
+setRowHeights(wb, 1, c(6, hdr_rows), 20)
+setRowHeights(wb, 1, week_rows, 20)
 
-openxlsx::addStyle(wb,1,header_st,4,c(1:6))
+header_st <- createStyle(fgFill = "#1D609D", textDecoration = "Bold", fontSize=10, fontColour = "#FFFFFF", valign="center")
+cell_st   <- createStyle(halign = "left", valign="center")
 
+addStyle(wb, 1, header_st,6,c(1:12))
 addStyle(wb, 1, style = titleStyle, rows = 1, cols = 1)
 addStyle(wb, 1, style = subtitleStyle, rows = 2, cols = 1)
 addStyle(wb, 1, style = linkStyle, rows = 3, cols = 1, stack = TRUE)
-addStyle(wb, 1, style = cell_st, cols = 1:6, rows = 5:nrow(prerelease_all)+4, gridExpand = TRUE, stack = TRUE)
+addStyle(wb, 1, style = bold_st2, rows = 4, cols=1, stack=TRUE)
+addStyle(wb, 1, style = cell_st, cols = 1:12, rows = 1:nrow(prerelease_all)+6, gridExpand = TRUE, stack = TRUE)
+addStyle(wb, 1, style = wrap_style, rows = 1:(nrow(prerelease_all)+6), cols = 8:10, gridExpand = TRUE, stack=TRUE)
+addStyle(wb, 1, style= border_left, rows = 7:(nrow(prerelease_all)+6), cols=13, gridExpand = TRUE, stack=TRUE)
+
 showGridLines(wb, 1, showGridLines = FALSE)
 
-saveWorkbook(wb, "Forward Look/Forward Look.xlsx", overwrite = TRUE, returnValue = FALSE)
+addFilter(wb, 1, rows=6, cols=1:12)
 
+saveWorkbook(wb, "Forward Look/Forward Look.xlsx", overwrite = TRUE, returnValue = FALSE)
